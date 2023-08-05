84a435d42eea3f4873b7b58a9060439769bd15a8
the-moezzat <dev.moezzat@gmail.com>
2023-08-05 20:28:15 +0300

    feat: join us form

---
+++ src/features/landing-page/FormJoin.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "../../ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../ui/form";
+import { Input } from "../../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../../ui/select";
+
+const FormSchema = z.object({
+  firstName: z.string().min(2, {
+    message: "first name must be at least 2 characters.",
+  }),
+  lastName: z.string().min(2, {
+    message: "last name must be at least 2 characters.",
+  }),
+  email: z.string().email("provide valid email"),
+  type: z.string({
+    required_error: "Please select an type.",
+  }),
+});
+
+export default function FormJoin() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex w-4/6 flex-col gap-4"
+      >
+        <div className="grid grid-cols-2 gap-4">
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className=" text-tertiary-3">First name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Michael" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className=" text-tertiary-3">Last name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Sutu" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className=" text-tertiary-3">Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Michael" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="type"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className=" text-tertiary-3">
+                How would you describe yourself?
+              </FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a type" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="buyer">Buyer</SelectItem>
+                  <SelectItem value="seller">Seller</SelectItem>
+                  <SelectItem value="both">both</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button size={"lg"} type="submit" className="mt-6 w-full">
+          Join for free
+        </Button>
+      </form>
+    </Form>
+  );
+}
--- src/features/landing-page/JoinUsForm.tsx
+++ src/features/landing-page/JoinUsForm.tsx
@@ -1,3 +1,5 @@
+import FormJoin from "./FormJoin";
+
 function JoinUs() {
   return (
     <div className=" bg-tertiary-9 p-16" id="join">
@@ -5,9 +7,11 @@ function JoinUs() {
         <p className=" mb-4 text-lg uppercase tracking-widest text-tertiary-2">
           Join us
         </p>
-        <h2 className="text-center text-5xl font-bold leading-normal text-tertiary-1">
+        <h2 className="mb-6 text-center text-4xl font-bold leading-normal text-tertiary-1">
           Don’t miss the opportunity to be part of sellsource
         </h2>
+
+        <FormJoin />
       </div>
     </div>
   );
