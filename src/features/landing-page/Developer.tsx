8bdc67ea51d9924bfd70f86f459200046c3c95a6
the-moezzat <dev.moezzat@gmail.com>
2023-08-06 21:09:30 +0300

    feat: developer section became responsive

--- src/features/landing-page/Developer.tsx
+++ src/features/landing-page/Developer.tsx
@@ -16,13 +16,21 @@ const Image = styled.img`
   object-position: center;
 `;
 
+const Section = styled.section`
+  display: grid;
+  grid-template-columns: repeat(24, 1fr);
+  gap: 1rem;
+  padding: 3rem 1.25rem;
+
+  @media (max-width: 1180px) {
+    gap: 0.5rem;
+  }
+`;
+
 function Developer() {
   return (
-    <div
-      className="grid grid-cols-[repeat(24,1fr)] grid-rows-[auto,auto] gap-4 bg-accent-6 px-5 py-12"
-      id="developer"
-    >
-      <div className="col-start-2 col-end-[13] row-span-full grid grid-cols-2 gap-4 max-lg:col-start-1">
+    <Section className="grid-rows-[auto,auto] bg-accent-6" id="developer">
+      <div className="col-start-2 col-end-[13] row-span-full grid grid-cols-2 gap-4 max-lg:col-start-1 max-[950px]:col-span-full max-[950px]:row-start-2 max-sm:row-start-3 max-sm:gap-2 max-[500px]:grid-cols-1">
         <DeveloperFeatureCard
           title="Simplify Sales Process"
           icon={<FastForward />}
@@ -46,12 +54,15 @@ function Developer() {
           text="Thrive in a supportive environment. SellSource empowers you."
         />
       </div>
-      <div className="col-start-13 col-end-[24] max-lg:col-end-[25]">
-        <Subheading className=" mb-2 text-accent-2">For developer</Subheading>
-        <h2 className="mb-4 text-4xl font-bold text-accent-10">
+
+      <div className="col-start-13 col-end-[24] max-lg:col-end-[25] max-[950px]:col-start-[13] max-[650px]:row-start-1 max-sm:col-span-full">
+        <Subheading className=" mb-2 text-accent-2 max-lg:mb-1">
+          For developer
+        </Subheading>
+        <h2 className="mb-4 text-4xl font-bold text-accent-10 max-lg:mb-2 max-lg:text-3xl">
           Where Developer Build for better world
         </h2>
-        <p className="mb-8 text-xl text-accent-9">
+        <p className="mb-8 text-xl text-accent-9 max-lg:mb-4 max-lg:text-base">
           Discover the Power of SellSource's Developer Tools for Unleashing Your
           Potential
         </p>
@@ -59,10 +70,11 @@ function Developer() {
           <Button className="">Join now for free</Button>
         </a>
       </div>
-      <div className="col-start-13 col-end-[24] h-64 overflow-hidden rounded-xl max-lg:col-end-[25]">
+
+      <div className="col-start-13 col-end-[24] h-64 overflow-hidden rounded-xl max-lg:col-end-[25] max-[950px]:col-start-1 max-[950px]:col-end-13 max-[950px]:row-start-1 max-sm:col-span-full max-sm:row-start-2">
         <Image src="/developer.jpg" />
       </div>
-    </div>
+    </Section>
   );
 }
 
--- src/features/landing-page/DeveloperFeatureCard.tsx
+++ src/features/landing-page/DeveloperFeatureCard.tsx
@@ -7,12 +7,14 @@ interface DeveloperFeatureProps {
 function DeveloperFeatureCard({ icon, title, text }: DeveloperFeatureProps) {
   return (
     <div className="flex flex-col items-start justify-between gap-4 rounded-2xl bg-accent-10 p-4">
-      <p className="text-2xl font-medium text-accent-1 ">{title}</p>
+      <p className="text-2xl font-medium text-accent-1 max-sm:text-xl">
+        {title}
+      </p>
       <div className="mt-auto space-y-2">
-        <div className="inline-block rounded-xl bg-accent-1 p-2 text-3xl text-accent-10 ">
+        <div className="inline-block rounded-xl bg-accent-1 p-2 text-3xl text-accent-10 max-sm:text-2xl">
           {icon}
         </div>
-        <p className="text-accent-3">{text}</p>
+        <p className="text-accent-3 max-sm:text-sm">{text}</p>
       </div>
     </div>
   );
