20c0c4f06a5d2a3a6c62902b99be915eae1d68da
the-moezzat <dev.moezzat@gmail.com>
2023-08-06 21:36:57 +0300

    feat: footer became responsive

--- src/features/landing-page/Footer.tsx
+++ src/features/landing-page/Footer.tsx
@@ -3,13 +3,13 @@ import { FacebookLogo, DiscordLogo } from "@phosphor-icons/react";
 function Footer() {
   return (
     <footer className="grid grid-cols-[repeat(24,1fr)] gap-4 p-4 pt-14">
-      <div className="col-start-2 col-end-9">
+      <div className="col-start-2 col-end-9 max-lg:col-start-1 max-md:col-end-12 max-[500px]:col-span-full">
         <img src="/logo.svg" alt="logo" className="mb-4 h-6" />
-        <p className="mb-6 text-lg text-gray-6">
+        <p className="mb-6 text-lg text-gray-6 max-md:text-sm max-sm:mb-3">
           Connect, Explore, and Thrive - Empowering Businesses and Developers
           with SellSource!
         </p>
-        <div className="flex items-center gap-6 text-3xl text-gray-7">
+        <div className="flex items-center gap-6 text-3xl text-gray-7 max-md:text-2xl">
           <FacebookLogo weight="fill" />
           <DiscordLogo weight="fill" />
           <svg
@@ -24,34 +24,39 @@ function Footer() {
           </svg>
         </div>
       </div>
-      <div className="col-start-9 col-end-[24] flex justify-end gap-14">
+
+      <div className="col-start-12 col-end-[24] flex justify-end gap-14 max-lg:col-end-[25] max-md:gap-7 max-[500px]:col-span-full max-[500px]:justify-start">
         <div className="space-y-4">
-          <p className="text-lg text-gray-7">Plan & Pricing</p>
-          <ul className="space-y-2 text-gray-6">
+          <p className="text-lg text-gray-7 max-sm:text-base">Plan & Pricing</p>
+          <ul className="space-y-2 text-gray-6 max-sm:text-xs">
             <li>Features</li>
             <li>news & blogs</li>
             <li>Careers</li>
           </ul>
         </div>
         <div className="space-y-4">
-          <p className="text-lg text-gray-7">About us</p>
-          <ul className="space-y-2 text-gray-6">
+          <p className="text-lg text-gray-7 max-sm:text-base">About us</p>
+          <ul className="space-y-2 text-gray-6 max-sm:text-xs">
             <li>Documentation</li>
             <li>Papers</li>
             <li>Press conference</li>
           </ul>
         </div>
       </div>
-      <div className="col-start-2 col-end-[24] space-y-4">
+      <div className="col-start-2 col-end-[24] space-y-4 max-lg:col-span-full">
         <div className="h-0.5 bg-gray-3"></div>
-        <div className="flex items-center justify-between">
-          <p className="text-sm text-gray-6">
+        <div className="flex items-center justify-between gap-2 max-md:flex-col-reverse">
+          <p className="text-sm text-gray-6 max-sm:text-xs">
             © 2023 sellsource.co All rights reserved.
           </p>
           <ul className="flex items-center gap-8">
-            <li className="text-sm text-gray-6">Terms of Service</li>
-            <li className="text-sm text-gray-6">Privacy Policy</li>
-            <li className="text-sm text-gray-6">Cookies</li>
+            <li className="text-sm text-gray-6 max-sm:text-xs">
+              Terms of Service
+            </li>
+            <li className="text-sm text-gray-6 max-sm:text-xs">
+              Privacy Policy
+            </li>
+            <li className="text-sm text-gray-6 max-sm:text-xs">Cookies</li>
           </ul>
         </div>
       </div>
