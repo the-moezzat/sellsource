926db270faa6bb39372395bde4ddcdf43935deb9
the-moezzat <dev.moezzat@gmail.com>
2023-08-06 23:25:07 +0300

    feat: hero secotion is finally responsive

--- src/features/landing-page/Hero.tsx
+++ src/features/landing-page/Hero.tsx
@@ -11,17 +11,21 @@ const Head = styled.header`
   /* min-height: calc(100vh - 68px); */
   padding: 96px 18px;
   overflow: hidden;
+
+  @media (max-width: 768px) {
+    padding: 52px 12px;
+  }
 `;
 
 function Hero() {
   return (
     <Head>
-      <h1 className="mb-10 text-center text-7xl font-bold text-tertiary-base">
+      <h1 className="mb-10 text-center text-7xl font-bold text-tertiary-base max-lg:text-6xl max-md:text-4xl">
         Empowering{" "}
         <span className="font-code text-primary-base">{"<Developers>"}</span>
         <br />
         Enabling{" "}
-        <span className="relative font-hand text-8xl font-normal text-primary-base">
+        <span className="relative font-hand text-8xl font-normal text-primary-base max-lg:text-7xl max-md:text-5xl">
           Businesses
           <img
             src="/hand-line.svg"
@@ -31,17 +35,17 @@ function Hero() {
         </span>
       </h1>
 
-      <p className="mb-16 w-2/5 text-center text-xl text-tertiary-4">
+      <p className="mb-16 w-2/5 text-center text-xl text-tertiary-4 max-lg:w-3/4 max-md:mb-10 max-md:text-base max-sm:w-11/12">
         Discover Hidden Gems: Unleash Software's Potential for Your Business
       </p>
 
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4 max-md:flex-col">
         <a href="#join">
           <Button size={"lg"} className="px-20 py-6 text-lg">
             Join us
           </Button>
         </a>
-        <div className="flex flex-col text-tertiary-5">
+        <div className="flex flex-col text-tertiary-5 max-md:text-sm">
           <p>
             <span className="font-bold">+500</span> business
           </p>
@@ -53,12 +57,12 @@ function Hero() {
       <img
         src="/circle-1.svg"
         alt="circle"
-        className=" absolute -bottom-8 -right-16 -z-10 h-96 translate-x-1/2 rotate-180"
+        className=" absolute -bottom-8 -right-16 -z-10 h-96 translate-x-1/2 rotate-180 max-md:h-72"
       />
       <img
         src="/circle-2.svg"
         alt="circle"
-        className=" absolute -left-24 top-2 h-96 -translate-x-1/2 "
+        className=" absolute -left-24 top-2 h-96 -translate-x-1/2 max-md:h-72 "
       />
     </Head>
   );
